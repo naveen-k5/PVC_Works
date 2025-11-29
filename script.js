46fad25b2fdc45e10773eba024444bff05dd04c9
Naveen <naveenkumarkannan811@gmail.com>
2025-11-29 17:33:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // Example: simple alert or form handler
 console.log("DoorWorks site loaded successfully!");
 
+<script>
+// Show popup after 1 second
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("offerPopup").style.display = "flex";
+    }, 1000);
+});
+
+// Close button
+document.getElementById("closePopup").addEventListener("click", () => {
+    document.getElementById("offerPopup").style.display = "none";
+});
+</script>
+
+
